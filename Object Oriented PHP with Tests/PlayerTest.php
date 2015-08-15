66aa29efe195619f90e713a282088427e7eabb4e
Anton Bevzuk <anton.bevzuk@smartstepgroup.com>
2015-08-15 21:05:35 +0300

    test commit from IDE

---
+++ Object Oriented PHP with Tests/PlayerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Антон
+ * Date: 15.08.2015
+ * Time: 20:56
+ */
+class PlayerTest extends PHPUnit_Framework_TestCase
+{
+
+    public function test()
+    {
+        assert(false);
+    }
+}
